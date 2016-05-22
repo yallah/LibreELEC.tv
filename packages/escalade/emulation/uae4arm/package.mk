bef4614c175798329c135e5d8da80bb63b8a9b9c
escalade <trondah@gmail.com>
2016-05-22 20:58:11 +0200

    uae4arm: update to f6a6c90

--- packages/escalade/emulation/uae4arm/package.mk
+++ packages/escalade/emulation/uae4arm/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="uae4arm"
-PKG_VERSION="e987881"
+PKG_VERSION="f6a6c90"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
