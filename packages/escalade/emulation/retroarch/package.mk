25a037dd271bed51b3deea0f7446ab84a299cf9c
escalade <trondah@gmail.com>
2016-06-04 22:22:10 +0200

    retroarch: update to 2f7bc87

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="bedd67a"
+PKG_VERSION="2f7bc87"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
